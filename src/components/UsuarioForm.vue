a209267c49a3b5f09f5a199d0f1eb77fe3479a2e
Lucas Sales <lucassales398@gmail.com>
2019-07-15 10:44:29 -0300

    cep api

--- src/components/UsuarioForm.vue
+++ src/components/UsuarioForm.vue
@@ -7,7 +7,7 @@
     <label for="senha">Senha</label>
     <input type="password" name="senha" id="senha" v-model="senha" />
     <label for="cep">Cep</label>
-    <input type="text" name="cep" id="cep" v-model="cep" />
+    <input type="text" name="cep" id="cep" v-model="cep" @keyup="preencherCep" />
     <label for="rua">Rua</label>
     <input type="text" name="rua" id="rua" v-model="rua" />
     <label for="numero">Numero</label>
@@ -26,6 +26,7 @@
 
 <script>
 import { mapFields } from "@/helpers.js";
+import { getCep } from "@/services.js";
 
 export default {
   name: "UsuarioForm",
@@ -45,6 +46,19 @@ export default {
       base: "usuario",
       mutation: "UPDATE_USUARIO"
     })
+  },
+  methods: {
+    preencherCep() {
+      const cep = this.cep.replace(/\D/g, "");
+      if (cep.length === 8) {
+        getCep(cep).then(response => {
+          this.rua = response.data.logradouro;
+          this.bairro = response.data.bairro;
+          this.estado = response.data.uf;
+          this.cidade = response.data.localidade;
+        });
+      }
+    }
   }
 };
 </script>
