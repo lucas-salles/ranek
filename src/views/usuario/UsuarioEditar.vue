0d695da93871f7ed32cadf102e786a8d6e2469d1
Lucas Sales <lucassales398@gmail.com>
2019-07-15 12:32:54 -0300

    usuário router

---
+++ src/views/usuario/UsuarioEditar.vue
@@ -0,0 +1,12 @@
+<template>
+  <p>Usuário Editar</p>
+</template>
+
+<script>
+export default {
+  name: "UsuarioEditar"
+};
+</script>
+
+<style>
+</style>
---
+++ src/views/usuario/UsuarioProdutos.vue
@@ -0,0 +1,12 @@
+<template>
+  <p>Usuário Produtos</p>
+</template>
+
+<script>
+export default {
+  name: "UsuarioProdutos"
+};
+</script>
+
+<style>
+</style>
