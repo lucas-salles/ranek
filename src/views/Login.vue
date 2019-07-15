155c438b8e51747ef46e0fd7a9f9340c3a3f43b5
Lucas Sales <lucassales398@gmail.com>
2019-07-15 09:32:40 -0300

    vuex login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -32,45 +32,46 @@ export default {
     };
   },
   methods: {
-      logar() {
-
-      }
+    logar() {
+      this.$store.dispatch("getUsuario", this.login.email);
+      this.$router.push({name: 'usuario'})
+    }
   }
 };
 </script>
 
 <style scoped>
 .login {
-    max-width: 500px;
-    margin: 0 auto;
-    padding: 0 20px;
+  max-width: 500px;
+  margin: 0 auto;
+  padding: 0 20px;
 }
 
 h1 {
-    text-align: center;
-    font-size: 2rem;
-    margin-top: 40px;
-    color: #87f;
+  text-align: center;
+  font-size: 2rem;
+  margin-top: 40px;
+  color: #87f;
 }
 
 form {
-    display: grid;
+  display: grid;
 }
 
 .btn {
-    width: 100%;
-    max-width: 300px;
-    margin-left: auto;
-    margin-right: auto;
+  width: 100%;
+  max-width: 300px;
+  margin-left: auto;
+  margin-right: auto;
 }
 
 .perdeu {
-    text-align: center;
-    margin: 20px auto 0 auto;
+  text-align: center;
+  margin: 20px auto 0 auto;
 }
 
 .perdeu a:hover {
-    color: #87f;
-    text-decoration: underline;
+  color: #87f;
+  text-decoration: underline;
 }
 </style>
---
+++ src/views/usuario/Usuario.vue
@@ -0,0 +1,12 @@
+<template>
+  <p>Página do Usuario: {{$store.state.usuario}}</p>
+</template>
+
+<script>
+export default {
+  name: "Usuario"
+};
+</script>
+
+<style>
+</style>
