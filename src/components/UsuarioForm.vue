e70bb3244d3e1f864abf140bc88327030d91f562
Lucas Sales <lucassales398@gmail.com>
2019-07-16 05:00:54 -0300

    transação

--- src/components/UsuarioForm.vue
+++ src/components/UsuarioForm.vue
@@ -1,11 +1,13 @@
 <template>
   <form>
-    <label for="nome">Nome</label>
-    <input type="text" name="nome" id="nome" v-model="nome" />
-    <label for="email">Email</label>
-    <input type="email" name="email" id="email" v-model="email" />
-    <label for="senha">Senha</label>
-    <input type="password" name="senha" id="senha" v-model="senha" />
+    <div class="usuario" v-if="mostrarDadosLogin">
+      <label for="nome">Nome</label>
+      <input type="text" name="nome" id="nome" v-model="nome" />
+      <label for="email">Email</label>
+      <input type="email" name="email" id="email" v-model="email" />
+      <label for="senha">Senha</label>
+      <input type="password" name="senha" id="senha" v-model="senha" />
+    </div>
     <label for="cep">Cep</label>
     <input type="text" name="cep" id="cep" v-model="cep" @keyup="preencherCep" />
     <label for="rua">Rua</label>
@@ -45,7 +47,10 @@ export default {
       ],
       base: "usuario",
       mutation: "UPDATE_USUARIO"
-    })
+    }),
+    mostrarDadosLogin() {
+      return !this.$store.state.login || this.$route.name === "usuario-editar";
+    }
   },
   methods: {
     preencherCep() {
@@ -64,10 +69,14 @@ export default {
 </script>
 
 <style scoped>
-form {
+form, .usuario {
   display: grid;
   grid-template-columns: 80px 1fr;
   align-items: center;
+}
+
+.usuario {
+  grid-column: 1 / 3;
 }
 
 .button {
--- src/views/Produto.vue
+++ src/views/Produto.vue
@@ -1,32 +1,40 @@
 <template>
   <section>
     <div v-if="produto" class="produto">
-        <ul class="fotos" v-if="produto.fotos">
-            <li v-for="(foto, index) in produto.fotos" :key="index">
-                <img :src="foto.src" :alt="foto.titulo">
-            </li>
-        </ul>
-        <div class="info">
-            <h1>{{produto.nome}}</h1>
-            <p class="preco">{{produto.preco | numeroPreco}}</p>
-            <p class="descricao">{{produto.descricao}}</p>
-            <button class="btn" v-if="produto.vendido === 'false'">Comprar</button>
-            <button v-else class="btn" disabled>Produto Vendido</button>
-        </div>
+      <ul class="fotos" v-if="produto.fotos">
+        <li v-for="(foto, index) in produto.fotos" :key="index">
+          <img :src="foto.src" :alt="foto.titulo" />
+        </li>
+      </ul>
+      <div class="info">
+        <h1>{{produto.nome}}</h1>
+        <p class="preco">{{produto.preco | numeroPreco}}</p>
+        <p class="descricao">{{produto.descricao}}</p>
+        <transition mode="out-in" v-if="produto.vendido === 'false'">
+          <button class="btn" v-if="!finalizar" @click="finalizar = true">Comprar</button>
+          <FinalizarCompra v-else :produto="produto" />
+        </transition>
+        <button v-else class="btn" disabled>Produto Vendido</button>
+      </div>
     </div>
-    <PaginaCarregando  v-else/>
+    <PaginaCarregando v-else />
   </section>
 </template>
 
 <script>
 import { api } from "@/services.js";
+import FinalizarCompra from "@/components/FinalizarCompra.vue";
 
 export default {
   name: "Produto",
   props: ["id"],
+  components: {
+    FinalizarCompra
+  },
   data() {
     return {
-      produto: null
+      produto: null,
+      finalizar: false
     };
   },
   methods: {
@@ -44,27 +52,27 @@ export default {
 
 <style scoped>
 .produto {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    grid-gap: 30px;
-    max-width: 900px;
-    padding: 60px 20px;
-    margin: 0 auto;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 30px;
+  max-width: 900px;
+  padding: 60px 20px;
+  margin: 0 auto;
 }
 
 .preco {
-    color: #e80;
-    font-weight: bold;
-    font-size: 1.5rem;
-    margin-bottom: 40px;
+  color: #e80;
+  font-weight: bold;
+  font-size: 1.5rem;
+  margin-bottom: 40px;
 }
 
 .descricao {
-    font-size: 1.2rem;
+  font-size: 1.2rem;
 }
 
 .btn {
-    margin-top: 60px;
-    width: 200px;
+  margin-top: 60px;
+  width: 200px;
 }
 </style>
